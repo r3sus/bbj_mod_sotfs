9df90f33c61718734d46e1d0ff5bdb717f87c473
pseudostripy <41550973+pseudostripy@users.noreply.github.com>
2021-02-07 00:03:06 +0000

    Tidy/polish

--- nobbjsotfs.c
+++ nobbjsotfs.c
@@ -45,11 +45,11 @@ void setup_d8proxy(void)
 
 void attach_hook(void)
 { 	
-	// This whole function is untidy and the variable names are terrible, sorry!
-	// One day I'll improve as a person and sort this out.
+	/* 	This whole function is untidy and the variable names are terrible, sorry!
+		One day I'll improve as a person and sort this out.
 	
-	// Here the inject/patch uses the scratch r10 register for dumping variables
-		
+		Here the inject/patch uses the scratch r10 register for dumping variables
+	*/
     void *base_addr = GetModuleHandle(NULL);
 	
 	/*
@@ -58,11 +58,6 @@ void attach_hook(void)
 	
 	static char spd_bytes[] =
 	{
-		//orig:
-		//0x84, 0xC0,										// test al,al                
-		//0x74, 0x07,               						// je 07 
-		//0x80, 0x8F, 0xE8, 0x00, 0x00, 0x00, 0x08,    	// or byte ptr [rdi+E8]
-		
 		//update jmp from dll (64bit)
 		0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 	// [0] mov r10, tempaddr_xxxxxxxx
 		0x49, 0x8b, 0x02, 												// mov rax, [r10] 
@@ -91,15 +86,14 @@ void attach_hook(void)
 			// inject:
 			{0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 	// mov r10, addr_xxxxxxxx
 			 0x41, 0xFF, 0xE2,												// [10] jmp r10
-			 0x90}, 														// [13] nop filler
-			// orig bytes:
-			{//0x84, 0xC0,                				// test al,al
-			 //0x74, 0x07,                				// je DarkSoulsII.exe+3EE3F9 
-			 //0x80, 0x8F, 0xE8, 0x00, 0x00, 0x00, 0x08,  // or byte ptr [rdi+e8]
-			 0x48, 0x8B, 0x05, 0xD0, 0xD4, 0x21, 0x01,  // mov rax,[DarkSoulsII.exe+160B8D0]
+			 0x90 															// [13] nop filler
+			},
+			 
+			// orig:
+			{0x48, 0x8B, 0x05, 0xD0, 0xD4, 0x21, 0x01,  // mov rax,[DarkSoulsII.exe+160B8D0]
 			 0x48, 0x8B, 0x58, 0x38,    				// mov rbx,[rax+38]
 			 0x48, 0x85, 0xDB 							// [11] test rbx,rbx
-			 }, 			
+			}, 			
 	    };
 		
 	void *spdinjaddr = base_addr + spd_patch.rel_addr;
@@ -113,12 +107,6 @@ void attach_hook(void)
 	memcpy(&spd_bytes[29],&spdinj_ret,sizeof(spdinj_ret)); 						// return to spdinject (+offset)
 	memcpy(&spd_bytes[2],&tmpaddr,sizeof(tmpaddr)); 							// fix mov rax,[DarkSoulsII.exe+160B8D0] 
 	memcpy(&spd_bytes[15],&BasePtr,sizeof(BasePtr)); 							// fix mov [BasePtr],rax 
-    
-		/*
-	char str[50];
-	sprintf(str,"baseptrsize %lld",sizeof(BasePtr));
-	MessageBox(0,str,"",0);
-	*/
 	
 	DWORD op;
 	VirtualProtect(spd_bytes, sizeof(spd_bytes),
@@ -160,7 +148,7 @@ void attach_hook(void)
 		0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // [57] mov r10, addr_xxxxxxxx
 		0x41, 0xFF, 0xE2, 											// jmp r10 
 	};
-
+	
 	void *jumphook = jump_bytes;
 	
 	struct patch jump_patch = 
@@ -170,27 +158,25 @@ void attach_hook(void)
 			// inject:
 			{0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 	// mov r10, addr_xxxxxxxx
 			 0x41, 0xFF, 0xE2,												// [10] jmp r10
-			 0x90, 0x90, 0x90}, 											// [13] nop filler
+			 0x90, 0x90, 0x90 												// [13] nop filler
+			}, 
+			
 			// orig:
 			{0xF3, 0x0F, 0x58, 0xC8, 				// addss xmm1,xmm0
 			 0x0F, 0xC6, 0xC9, 0x00,            	// shufps xmm1,xmm1,00
 			 0x0F, 0x51, 0xC1,     					// sqrtps xmm0,xmm1
-			 0x0F, 0x29, 0x44, 0x24, 0x20},       	// movaps [rsp+20],xmm0
+			 0x0F, 0x29, 0x44, 0x24, 0x20 			// movaps [rsp+20],xmm0
+			},       	
 	    };
 	
 	void *jumpinjaddr = base_addr + jump_patch.rel_addr;
 	void *jumpinj_ret = jumpinjaddr + jump_patch.size;
 	
-	//DWORD temprel = 0x160B8D0;
-	//void *tmpaddr = base_addr + temprel;
-	
 	addrToJumpTo64 = (uint64_t) jumphook;
 	memcpy(&jump_patch.patch[2], &addrToJumpTo64, sizeof(addrToJumpTo64)); 		// jmp fw
 	memcpy(&jump_bytes[59],&jumpinj_ret,sizeof(jumpinj_ret)); 					// return to jumpinject (+offset)
-	
 	memcpy(&jump_bytes[14],&BasePtr,sizeof(BasePtr)); 							// fix mov rax,[BasePtr]
 	
-		
 	// Edit memory:
 	size = jump_patch.size;
 	if (memcmp(jumpinjaddr, jump_patch.orig, size) == 0) {
@@ -199,7 +185,6 @@ void attach_hook(void)
 		memcpy(jumpinjaddr, jump_patch.patch, size);
 		VirtualProtect(jumpinjaddr, size, old, &old);
 	}
-	
 	
 }
 
